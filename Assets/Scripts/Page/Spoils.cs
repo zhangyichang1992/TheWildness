0acd517e74079a1c94bac5a3f820a6a365f8931a
zhang19920408 <790435280@qq.com>
2018-06-13 16:12:00 +0800

    6-13提交

---
+++ Assets/Scripts/Page/Spoils.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Global;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spoils : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        #region 继续
+        Button btnSkip = GameObject.Find("CanvasSkills/BtnSkip").GetComponent<Button>();
+        btnSkip.onClick.AddListener(delegate ()
+        {
+            GameInfo.NewStage();
+        });
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/Scripts/UI/SpoilUpdater.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.UI
+{
+    public static  class SpoilUpdater
+    {
+        /// <summary>
+        /// 更改战利品界面UI
+        /// </summary>
+        /// <param name="money">获得金钱</param>
+        /// <param name="exp">获得经验</param>
+        /// <param name="propName">获得宝物</param>
+        /// <param name="skillType">获得的技能类型</param>
+        public static void Refresh(int money, int exp, PropName propName, SkillType skillType)
+        {
+        }
+    }
+}
